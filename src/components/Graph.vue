460640e09f5a012187ddc698631a611506b2eb38
seiei-n <solehamugoiyo@gmail.com>
2022-04-30 22:05:48 +0900

    Add functions

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,36 +1,91 @@
 <template>
   <div>
-    <h1>todouhukenn</h1>
-    <div v-for="prefecture in prefectures" :key="prefecture" class="prefecture">
-      <input type="checkbox" v-model="checked" @change="selectPrefecture" />
-      <label>{{ prefecture.prefName }}</label>
+    <h1>都道府県</h1>
+
+    <div class="prefecture">
+      <label v-for="prefecture in st.prefectures" :key="prefecture.prefCode">
+        <input
+          type="checkbox"
+          :id="prefecture.prefCode"
+          :value="prefecture.prefCode"
+          @click="isChecked(prefecture.prefCode, $event.target.checked)"
+        />
+        {{ prefecture.prefName }}
+        {{ prefecture.prefCode }}
+      </label>
     </div>
+    <vue-highcharts :options="chartOptions" />
   </div>
 </template>
 
 <script>
-import { onMounted, reactive } from '@vue/runtime-core';
+import VueHighcharts from 'vue3-highcharts';
+import { defineComponent, onMounted, reactive } from 'vue';
 import axios from 'axios';
-export default {
+export default defineComponent({
+  components: {
+    VueHighcharts
+  },
   setup() {
+    onMounted(() => {
+      getPref();
+    });
     const url = process.env.VUE_APP_URL;
     const api_key = process.env.VUE_APP_API_KEY;
-    const state = reactive({
-      res: null,
+    const st = reactive({
       prefectures: null,
       prefname: null,
-      prefcode: null
+      prefcode: null,
+      checkedpref: [],
+      populations: null,
+      checked: false
     });
+    const logger = prefcode => {
+      console.log(prefcode);
+      getPopulation(prefcode);
+      console.log(st.populations);
+    };
 
-    async function fetchPref() {
-      state.res = await axios.get(url + '/prefectures', {
+    const getPref = async () => {
+      st.res = await axios.get(url + '/prefectures', {
         headers: { 'X-API-KEY': api_key }
       });
-      state.prefectures = state.res.data.result;
-    }
+      st.prefectures = st.res.data.result;
+    };
 
-    onMounted(fetchPref);
-    return state;
+    const getPopulation = async prefcode => {
+      const res = await axios.get(
+        url + '/population/composition/perYear?cityCode=-&prefCode=' + prefcode,
+
+        {
+          headers: { 'X-API-KEY': api_key }
+        }
+      );
+      st.populations = res.data.result.data[0].data[0].value;
+    };
+
+    const isChecked = (checkedpref, checked) => {
+      if (checked) {
+        st.checkedpref.push(checkedpref);
+      } else {
+        st.checkedpref = st.checkedpref.filter(pref => pref !== checkedpref);
+      }
+
+      console.log(st.checkedpref);
+      console.log(checked);
+    };
+
+    const renderGraph = () => {};
+
+    const chartOptions = {};
+    return {
+      st,
+      logger,
+      getPopulation,
+      isChecked,
+      renderGraph,
+      chartOptions
+    };
   }
-};
+});
 </script>
